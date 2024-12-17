84199681c3c0c76a8cbd5640f34383664612e6eb
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-12-18 00:03:48 +0300

    created screens

---
+++ lib/screens/catalogue_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CatalogueScreen extends StatefulWidget {
+  const CatalogueScreen({super.key});
+
+  @override
+  State<CatalogueScreen> createState() => _CatalogueScreenState();
+}
+
+class _CatalogueScreenState extends State<CatalogueScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
