6362fdac81d662ca71c2e3a5a69b9b216ff675f7
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-12-17 23:51:59 +0300

    color class

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color pink100 = Color(0xFFFDF4F6);
+  static const Color pink200 = Color(0xFFFFE3EB);
+  static const Color pink300 = Color(0xFFF9D3DD);
+  static const Color pink400 = Color(0xFFEE728A);
+  static const Color pink500 = Color(0xFFE84A69);
+
+  static const Color green200 = Color(0xFFC2E8DC);
+  static const Color green300 = Color(0xFFC2E8DC);
+  static const Color green500 = Color(0xFF225C4B);
+
+  static const Color black100 = Color(0xFFF9F9F9);
+  static const Color black200 = Color(0xFFF2F2F2);
+  static const Color black300 = Color(0xFFE3E3E3);
+  static const Color black400 = Color(0xFF737373); 
+
+  static const Color additional1 = Color(0xFFECE5FF);
+  static const Color additional2 = Color(0xFFE3EBF6);
+  static const Color additional3 = Color(0xFFDCF2DE);
+  static const Color additional4 = Color(0xFFF9F5D3);
+}
